702721308a5c55f224ca6e1bfe433536184528eb
Giuseppeg34 <66140622+Giuseppeg34@users.noreply.github.com>
2021-04-15 18:15:08 +0200

    Add files via upload

--- Runtime/Scripttest.cs
+++ Runtime/Scripttest.cs
@@ -7,7 +7,7 @@ public class Scripttest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // CIAONE
+        
     }
 
     // Update is called once per frame
